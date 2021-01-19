c8d1f07985f9c0309661c1ab4d2655aa8a90ff07
Marek Zdybel <marek@motta.com.pl>
2021-01-19 11:12:15 +0100

    refs #334 add info about datetime when error occurred

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from CheckSiteStatus import CheckSiteStatus
 from SendMail import SendMail
+from datetime import datetime
 import configparser
 
 
 config = configparser.ConfigParser()
-config.read("/home/userdir/scripts/WebPageStatusCheck/config.ini")
+config.read("config.ini")
 site_list = config.get("URL", "SITE_LIST")
 site_list = site_list.split(',')
 
@@ -12,8 +13,12 @@ if __name__ == '__main__':
     for url in site_list:
         page_status = CheckSiteStatus.check_site_url(url)
 
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
         if page_status == 200:
-            print('Site {} working correctly...'.format(url))
+            print('{}: Site {} working correctly...'.format(dt_string, url))
         else:
             mail = SendMail(url, page_status)
             mail.send_notification_if_error()
+            print('{}: Error occurred on site {}...'.format(dt_string, url))
